ebf7a1b1c5551d19404d6e385136404b470c85b2
unknown <sandirpl3e@gmail.com>
2019-10-07 09:43:12 +0700

    Complete Qr Code

--- app/src/main/java/com/codewith/sandisuryadi/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/codewith/sandisuryadi/qrcodescanner/MainActivity.java
@@ -1,13 +1,111 @@
 package com.codewith.sandisuryadi.qrcodescanner;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import androidx.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView ivBgContent;
+    private CodeScanner mCodeScanner;
+    private CodeScannerView scannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    ivBgContent = findViewById(R.id.ivBgContent);
+    scannerView = findViewById(R.id.scannerView);
+
+    ivBgContent.bringToFront();
+
+    mCodeScanner = new CodeScanner(this, scannerView);
+    mCodeScanner.setDecodeCallback(new DecodeCallback() {
+        @Override
+        public void onDecoded(@NonNull final Result result) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String message = "result :\n" + result.getText();
+                    showAlerDialog(message);
+                }
+            });
+        }
+    });
+    checkCameraPermisson();
+ }
+
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        checkCameraPermisson();
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+    }
+
+    private void checkCameraPermisson(){
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        mCodeScanner.startPreview();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .check();
+    }
+
+    private void showAlerDialog (String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(
+                "SCAN LAGI",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        mCodeScanner.startPreview();
+                    }
+                });
+        builder.setNegativeButton(
+                "CANCEL",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
